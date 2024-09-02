8b2603a409981a016bf9564fa548d04ea3d3e9f2
Marina Yavkina <marinayavkina@MacBook-Pro-Marina.local>
2024-09-02 21:03:31 +0300

    Added part about myself

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -8,7 +8,13 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Текст писать тут
+        <ul>
+        <li>Самостоятельно уметь создавать веб приложения с использованием react и разворачивать на сервере.
+        Освоить современые технологии разработкию.</li>
+        <li>Планирую освоить разрабротку фронта на react, работу с figma , а также навыки верстки и стилизации  приложений.</li>
+        <li>Владею навыками работы asp.net core 6/8, postgresql ,java script , typescript, angular.</li>
+        <li>Опыт работы с delphi ,postgresql(разработка медицинских бд), asp.net 5, asp.net core 6, 8, angular.</li>
+        </ul>
         </p>
       </header>
     </div>
