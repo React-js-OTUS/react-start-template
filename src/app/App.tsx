62162a1a46723268384ee008e2f984638164cb6a
Marina Yavkina <marinayavkina@MacBook-Pro-Marina-2.local>
2024-10-12 23:06:17 +0300

    fix

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,23 +1,16 @@
 import React from 'react';
 import logo from './logo.svg';
-import './App.css';
-import { Layout } from './components/layout.module';
-import { Header } from './components/header.module';
-import { Logo } from './components/logo.module';
-import { Operation } from './components/operation/operation';
-import {ImageSlider}  from './components/custom-slider/image.slider';
-import access from  "/Users/marinayavkina/Documents/react_course/hw2/MarinaBastion.github.io/src/stories/assets/code-brackets.svg";
-import gith from "/Users/marinayavkina/Documents/react_course/hw2/MarinaBastion.github.io/src/app/assets/icons8-logo.svg"
+import Styles from './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header"> 
-        <img src={logo} className="App-logo" alt="logo" />
+    <div className={Styles.App}>
+      <header className={Styles.Appheader}>
+        <img src={logo} className={Styles.App_logo} alt="logo" />
         <p>Явкина Марина Александровна
         <ul>
         <li>Самостоятельно уметь создавать веб приложения с использованием react и разворачивать на сервере.
-        Освоить современые технологии разработкию.</li>
+        Освоить современые технологии разработки.</li>
         <li>Планирую освоить разрабротку фронта на react, работу с figma , а также навыки верстки и стилизации  приложений.</li>
         <li>Владею навыками работы asp.net core 6/8, postgresql ,java script , typescript, angular.</li>
         <li>Опыт работы с delphi ,postgresql(разработка медицинских бд), asp.net 5, asp.net core 6, 8, angular.</li>
