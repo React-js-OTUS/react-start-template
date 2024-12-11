c06008e8cd6b322277ef47cce64f7e258dea2172
Anton Rokunec <rokunecav@rtkait.ru>
2024-12-11 18:05:30 +0300

    homework_1: homework done

--- src/homeworks/ts1/3_write.ts
+++ src/homeworks/ts1/3_write.ts
@@ -5,6 +5,51 @@
  * В целом сделайте так, как вам будет удобно.
  * */
 
+class Category {
+  constructor(public id: string, public name: string, public photo?: string) {}
+}
+
+class Product {
+  constructor(
+    public id: string,
+    public name: string,
+    public photo: string,
+    public price: number,
+    public createdAt: string,
+    public category: Category,
+    public desc?: string,
+    public oldPrice?: number
+  ) {}
+}
+
+class Cost {
+  constructor(
+    public id: string,
+    public name: string,
+    public amount: number,
+    public category: Category,
+    public createdAt: string,
+    public type: 'Cost',
+    public desc?: string,
+  ) {
+  }
+}
+
+class Profit {
+  constructor(
+    public id: string,
+    public name: string,
+    public amount: number,
+    public category: Category,
+    public createdAt: string,
+    public type: 'Profit',
+    public desc?: string,
+  ) {
+  }
+}
+
+
+type Operation = Cost | Profit;
 /**
  * Нужно создать тип Category, он будет использоваться ниже.
  * Категория содержит
@@ -47,10 +92,41 @@
  * Создает случайный продукт (Product).
  * Принимает дату создания (строка)
  * */
-// export const createRandomProduct = (createdAt: string) => {};
+
+export const createRandomProduct = (createdAt: string): Product => {
+  return new Product(
+    new Date().toISOString(),
+    'Product',
+    'https://cs5.pikabu.ru/post_img/big/2015/06/04/11/1433446202_1725992411.jpg',
+    100,
+    createdAt,
+    new Category(new Date().toISOString(), 'Category')
+  );
+};
 
 /**
  * Создает случайную операцию (Operation).
  * Принимает дату создания (строка)
  * */
-// export const createRandomOperation = (createdAt: string) => {};
+export const createRandomOperation = (createdAt: string): Operation => {
+  const randomNumber = Math.floor(+Math.random().toFixed(2));
+  if (randomNumber > 0.48) {
+    return new Cost(
+      new Date().toISOString(),
+      'CostName',
+      100,
+      new Category(new Date().toISOString(), 'Category'),
+      createdAt,
+      'Cost'
+    );
+  } else {
+    return new Profit(
+      new Date().toISOString(),
+      'ProfitName',
+      100,
+      new Category(new Date().toISOString(), 'Category'),
+      createdAt,
+      'Profit'
+    );
+  }
+};
